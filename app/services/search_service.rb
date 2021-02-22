1e4558d35c9733c26de6215934d3f349c9c120a1
Hans Gamarra <hans.gamarra1303@gmail.com>
2021-02-22 17:47:09 -0500

    avoiding duplicate results on both engine results

--- app/services/search_service.rb
+++ app/services/search_service.rb
@@ -21,9 +21,28 @@ class SearchService
 
       {
         engine: ENGINES[:both],
-        items: google_result[:items].concat(bing_result[:items]),
+        items: merge_results(google_result, bing_result),
         pagination: { page: @page, per_page: 10 }
       }
     end
+  end
+
+  private
+
+  def merge_results(search_result1, search_result2)
+    merged_result = search_result1[:items]
+    search_result_urls = search_result1[:items].map { |search_result| search_result[:url] }
+
+    search_result2[:items].each do |search_result|
+      duplicated_result_index = search_result_urls.index(search_result[:url])
+      if duplicated_result_index
+        merged_result[duplicated_result_index][:duplicate_count] ||= 0
+        merged_result[duplicated_result_index][:duplicate_count] += 1
+      else
+        merged_result << search_result
+      end
+    end
+
+    merged_result
   end
 end
--- spec/services/search_service_spec.rb
+++ spec/services/search_service_spec.rb
@@ -52,8 +52,53 @@ RSpec.describe SearchService do
     end
 
     context 'when engine is both' do
-      it "returns the google and bing search service results" do
-        expect(described_class.new('test', engine: described_class::ENGINES[:both]).call[:items].count).to eq(3)
+      context "when search results are different" do
+        it "returns the google and bing search service results" do
+          expect(described_class.new('test', engine: described_class::ENGINES[:both]).call[:items].count).to eq(3)
+        end
+      end
+      
+      context 'when there are similar search results' do
+        before do
+          allow(Google::Api).to receive(:search).and_return({
+            'items': [
+              {
+                'link': 'http://google.com',
+                'title': 'Google',
+                'snippet': 'Just another mock',
+                'displayLink': 'google.com'
+              }
+            ]
+          }.with_indifferent_access)
+          allow(Bing::Api).to receive(:search) do
+            {
+              'webPages': {
+                'value': [
+                  {
+                    'url': 'http://bing.com',
+                    'name': 'Bing',
+                    'snippet': 'Just another mock',
+                    'displayUrl': 'bing.com'
+                  },
+                  {
+                    'url': 'http://google.com',
+                    'name': 'Google',
+                    'snippet': 'Just another mock',
+                    'displayLink': 'google.com'
+                  }
+                ]
+              }
+            }.with_indifferent_access
+          end
+        end
+
+        it "returns the google and bing search service results without duplicates" do
+          expect(described_class.new('test', engine: described_class::ENGINES[:both]).call[:items].count).to eq(2)
+        end
+        
+        it "returns a duplicate_count for the duplicated search result" do
+          expect(described_class.new('test', engine: described_class::ENGINES[:both]).call[:items][0][:duplicate_count]).to eq(1)
+        end
       end
     end
   end
